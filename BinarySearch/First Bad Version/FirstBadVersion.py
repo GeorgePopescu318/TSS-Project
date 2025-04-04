8d64ddbf0136c8b69d357084da79036287436287
Anca Alexandru <104026308+alexalex200@users.noreply.github.com>
2025-04-04 12:49:08 +0300

    Delete BinarySearch directory

--- BinarySearch/First Bad Version/FirstBadVersion.py
+++
@@ -1,9 +0,0 @@
-#https://github.com/gahogg/Leetcode-Solutions/blob/main/First%20Bad%20Version%20-%20Leetcode%20278/First%20Bad%20Version%20-%20Leetcode%20278.py
-
-def isBadVersion(version: int) -> bool:
-    return version >= 4
-
-def firstBadVersion(self, n: int) -> int:
-        for version in range(1, n + 1):
-            if isBadVersion(version):
-                return version
